29492b939895a68ba3e12a908cd184515a51f3fc
waltermaz <happy8D@gmail.com>
2021-02-18 11:47:01 -0800

    using source hello-world.go file

--- hello-world.go
+++ hello-world.go
@@ -1,8 +1,5 @@
 # add a new comment
-# first comment into file
 # add a second comment
-# add a third comment
-# add a fourth comment
 # file:  hello-world.go
 # description:  my very first go program
 
